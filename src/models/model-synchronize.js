90a2af2263e027e967b5412960af2cfa6c5d2ba7
alliano <allianoanonymous@gmail.com>
2024-10-25 15:12:12 +0700

    setup CI

--- src/models/model-synchronize.js
+++ src/models/model-synchronize.js
@@ -1,5 +1,8 @@
-const MODELMERGE = [
+import HistoryTindakanModel from "./history-tindakan-model.js";
+import InstalasiGawatDaruratModel from "./instalasi-gawat-darurat-model.js";
+import PetugasTindakanModel from "./petugas-tindakan-model.js";
+import UserModel from "./user-model.js";
 
-];
+const MODELMERGE = [HistoryTindakanModel, InstalasiGawatDaruratModel, PetugasTindakanModel, UserModel];
 
 export default MODELMERGE;
